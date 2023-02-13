2aaaf3e20325307ae66d93bbf19e56691aa35478
curtis <curtis@fillygaming888.com>
2023-02-13 10:04:29 +0800

    Update

---
+++ precache-manifest.71b69c2bf4a16ce2545a5db37e4bfd59.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b1cf4c835cb6e640ed3",
+    "url": "/otp_counter/css/app.c2069fa4.css"
+  },
+  {
+    "revision": "bd45ed8af1cb3030e181",
+    "url": "/otp_counter/css/chunk-vendors.b0c0533d.css"
+  },
+  {
+    "revision": "2d1eb765335816d2ee2a128eec8978be",
+    "url": "/otp_counter/fonts/Lexend-Medium.2d1eb765.ttf"
+  },
+  {
+    "revision": "fd2d8ea04e25585558f2396e4fa56c06",
+    "url": "/otp_counter/fonts/Lexend-Regular.fd2d8ea0.ttf"
+  },
+  {
+    "revision": "59fd34224fa2b3a42647fc9d459b64cb",
+    "url": "/otp_counter/index.html"
+  },
+  {
+    "revision": "6b1cf4c835cb6e640ed3",
+    "url": "/otp_counter/js/app.8b92a9c5.js"
+  },
+  {
+    "revision": "0d0defdfb31f41fb4f51",
+    "url": "/otp_counter/js/chunk-2d23028a.492c8fed.js"
+  },
+  {
+    "revision": "239f0c06fe849362ee81",
+    "url": "/otp_counter/js/chunk-860f3626.c83b452f.js"
+  },
+  {
+    "revision": "bd45ed8af1cb3030e181",
+    "url": "/otp_counter/js/chunk-vendors.3db45850.js"
+  },
+  {
+    "revision": "ff3f4520ee1c52857a5816c5c41a499d",
+    "url": "/otp_counter/manifest.json"
+  }
+]);
